be53205a74673bb3111c415c7b66d6fc1ace6a59
David Winslow <cdwinslow@gmail.com>
2020-01-09 20:56:30 -0500

    Use docker build stages; get polygon import working

--- Dockerfile
+++ Dockerfile
@@ -1,38 +1,44 @@
-FROM ubuntu:19.04
-RUN apt-get update && \
-    DEBIAN_FRONTEND=noninteractive apt-get install -yq gdal-bin vim git build-essential libsqlite3-dev \
-	    zlib1g-dev golang-go curl python csvkit
+# Build stage: runtime_packages. Share the results of apt-get update across the dev/production images.
+FROM ubuntu:19.04 AS runtime_packages
+RUN apt-get update && apt-get install -y python gdal-bin jq libsqlite3-0 zlib1g
 
-# Create a directory for tippecanoe sources.
-RUN mkdir -p /tmp/tippecanoe-src
+# Build stage: build_environment. Sources and build tools not needed for the ultimate production image.
+FROM runtime_packages AS build_environment
+RUN apt-get install -y curl git build-essential libsqlite3-dev zlib1g-dev
+RUN curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs -o /tmp/rustup.sh && \
+    sh -- /tmp/rustup.sh -y --profile=minimal
+
+## Begin tippecanoe setup steps.
 RUN git clone https://github.com/mapbox/tippecanoe.git /tmp/tippecanoe-src
+RUN cd /tmp/tippecanoe-src && make && make install
+## Tippecanoe fully built at this point.
 
-# Build tippecanoe.
-WORKDIR /tmp/tippecanoe-src
-RUN make && make install
-WORKDIR /
+## Begin xsv setup steps.
+RUN ~/.cargo/bin/cargo install xsv
+## xsv fully built at this point.
 
-# Remove source dirs and all build tools.
-RUN rm -rf /tmp/tippecanoe-src \
-  && apt-get -y remove --purge build-essential && apt-get -y autoremove
-
-# Copy scripts to generate maptiles.
-RUN mkdir -p /maptiles
-COPY scripts/prep-geojson-input-bq2tiles_ogr_csvkit.sh /maptiles
-COPY queries/query-bqsj.sql /maptiles
-COPY templates/convert.jsonnet /maptiles
-COPY maptiles/example.html /maptiles
-WORKDIR /maptiles
-
-# Install gcloud SDK and command line tools.
+## Install gcloud SDK and command line tools.
 RUN curl https://sdk.cloud.google.com | bash
-ENV PATH="/root/google-cloud-sdk/bin:${PATH}"
+# gcloud installed, leaving some configuration steps for production image.
+
+## Final build stage, containing tools and scripts needed in production job.
+FROM runtime_packages
+
+# Copy compiled binaries from build image.
+COPY --from=build_environment \
+    /root/.cargo/bin/xsv /usr/local/bin/* \
+    /root/bin/
+COPY --from=build_environment /root/google-cloud-sdk /root/
+ENV PATH="${PATH}:/root/google-cloud-sdk/bin:/root/bin"
 
 RUN gcloud config set project measurement-lab
-
 # NOTE: the bq cli leverages the gcloud auth, however still must perform an
 # authentication initialization on the first run. This initialization also
 # generates an unconditional "Welcome to BigQuery!" preamble message, which
 # corrupts the remaining json output. The following command attempts to list a
 # fake dataset which runs through the auth initialization and welcome message.
 RUN bq --headless --project_id measurement-lab ls fake-dataset &> /dev/null || :
+
+# Copy scripts to generate maptiles.
+COPY  maptiles queries schemas scripts templates /maptiles/
+WORKDIR /maptiles
--- scripts/prep-geojson-input-bq2tiles_ogr_xsv.sh
+++ scripts/prep-geojson-input-bq2tiles_ogr_xsv.sh
@@ -2,51 +2,61 @@
 
 set -eux
 
-PROJECT=${1:?Please provide a GCP project for tile upload}
+PROJECT="${1:?Please provide a GCP project for tile upload}"
+TABLE="georgia.nc_counties"
+QUALIFIED_TABLE="${PROJECT}:${TABLE}"
+QUERIES="${QUERIES:-/maptiles}"
+SCRIPTS="${SCRIPTS:-/maptiles}"
+SCHEMAS="${SCHEMAS:-/maptiles}"
 
 # TODO: replace use of bq with a custom Go binary that runs the query and
 # outputs the right format. Also open an HTTP port for Cloud Run.
 
 # Make a temporary table using a schema definition based on the expected query output fields 
 bq mk --table --expiration 3600 --description "temp table for bq2maptiles process" \
-	mlab-sandbox:critzo.nc_counties schemas/nc_counties_spjoin.json
+	"${QUALIFIED_TABLE}" "${SCHEMAS}"/nc_counties_spjoin.json
 
-# Series of several commands below tied together with && so the previous command must finish
-# successfully before proceeding. 
-# - Run bq query with generous row limit. Write results to temp table created above.
-#   NOTE: bq converts all types to strings, including ints and floats.
-# - Extract contents of the temp table to a gcs storage location, using wildcard sharding
-#   to accomodate large results.
-# - Merge sharded results into one CSV
-# - Download the merged CSV locally to use to generate tiles.
+# Run bq query with generous row limit. Write results to temp table created above.
+# By default, bq fetches the query results to display in the shell, consuming a lot of memory.
+# Use --nosync to "fire-and-forget", then implement our own wait loop to defer the next command
+# until the table is populated.
+JOB_ID=$(bq --nosync --project_id "${PROJECT}" query \
+	--allow_large_results --destination_table "${QUALIFIED_TABLE}" \
+    --replace --use_legacy_sql=false --max_rows=4000000 "$(cat "${QUERIES}/query-bqsj.sql")")
+JOB_ID="${JOB_ID#Successfully started query }"
 
-bq --project_id measurement-lab query \
-	--allow_large_results --destination_table mlab-sandbox:critzo.nc_counties \
-    --replace --use_legacy_sql=false --max_rows=4000000 "`cat queries/query-bqsj.sql`" && \
-    bq extract --destination_format CSV 'mlab-sandbox:critzo.nc_counties' \
-    gs://bigquery-maptiles-mlab-sandbox/csv/nc_counties_*.csv && \
-    gsutil compose gs://bigquery-maptiles-mlab-sandbox/csv/nc_counties_* \
-    gs://bigquery-maptiles-mlab-sandbox/csv/nc_counties_merged.csv && \
-    gsutil cp gs://bigquery-maptiles-mlab-sandbox/csv/nc_counties_merged.csv \
-    maptiles/results.csv
+until [ DONE == $(bq --format json show --job "${JOB_ID}" | jq -r '.status.state') ]
+do
+  sleep 30
+done 
 
-# Cleanup the files on GCS because we don't need them there anymore.
-#sutil rm gs://bigquery-maptiles-mlab-sandbox/csv/nc_counties_*
+# Generate CSV files; expected to include geometry info in WKT format.
+bq extract --destination_format CSV "${QUALIFIED_TABLE}" \
+    gs://bigquery-maptiles-mlab-sandbox/csv/nc_counties_*.csv
 
-# For geo-joining using a shapefile and results using ogr2ogr
-#ogr2ogr -f GeoJSON nc_2018_county.geojson geo/north_carolina_county_cb2018_500k.shp
-#ogr2ogr -f GeoJSON mlab_nc_2018_county.geojson results.csv -sql \
-#	"SELECT * FROM results c JOIN 'geo/north_carolina_county_cb2018_500k.shp'.north_carolina_county_cb2018_500k s on c.geo_id = s.GEOID"
+# Fetch the CSV files that were just exported.
+gsutil -m cp gs://bigquery-maptiles-mlab-sandbox/csv/nc_counties_*.csv ./
 
-# ogr2ogr+tippecanoe to handle the csv > tiles
-#ogr2ogr -f GeoJSON /dev/stdout \
-#  -oo KEEP_GEOM_COLUMNS=no \
-#  results.csv | \
-#  tippecanoe -o maptiles/mlab_nc_2018_county.mbtiles \
-#  -l mlab_nc_2018_county /dev/stdin -zg
-#  tippecanoe -e maptiles/nc_counties -f -l nc_counties /dev/stdin -z6 \
-#  --simplification=10 --detect-shared-borders \
-#  --coalesce-densest-as-needed --no-tile-compression
+#Cleanup the files on GCS because we don't need them there anymore.
+# gsutil rm gs://bigquery-maptiles-mlab-sandbox/csv/nc_counties_*
+
+# ogr2ogr requires a schema file to know which csv column represents
+# the geometry. We pass all filenames to the inference script, but
+# it only reads the first one, since the schema should be consistent
+# for all of them.
+$SCRIPTS/infer_csvt_schema.sh nc_counties_*.csv > schema.csvt
+
+# Use xargs to convert all the csv files to geojson individually, in
+# parallel. We will aggregate them in the next step.  See csv_to_geojson
+# script for ogr2ogr args.
+echo nc_counties_*.csv | xargs -n1 -P4 $SCRIPTS/csv_to_geojson.sh 
+
+# Let tippecanoe read all the geojson files into one layer.
+tippecanoe -e maptiles/nc_counties -f -l nc_counties *.geojson -z6 \
+    --simplification=10 \
+    --detect-shared-borders \
+    --coalesce-densest-as-needed \
+    --no-tile-compression
 
 #upload to cloud storage box
 #gsutil -m -h 'Cache-Control:private, max-age=0, no-transform' \
